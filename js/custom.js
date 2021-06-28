74954ef3c6fdf2c14656ede6f5552b250e31b3f1
shajjad-shuvo-cit <shajjad.cit.bd@gmail.com>
2021-06-28 13:13:10 +0600

    file added

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(function(){
+   $('.banner-slider').slick({
+       arrows: false,
+       dots: true,
+       slidesToScroll: 1,
+       autoplay: true,
+       autoplaySpeed: 5000,
+   }); 
+});
