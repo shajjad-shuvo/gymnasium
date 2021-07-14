ca71813ec6c3ed9faf3ef3cd7fd43dcbcc88a3e2
shajjad-shuvo <shajjad.shuvo84@gmail.com>
2021-07-14 20:36:05 +0600

    resonsive

--- js/custom.js
+++ js/custom.js
@@ -16,5 +16,80 @@ $(function(){
         arrows: false,
         slidesToScroll: 1,
         slidesToShow: 4,
+        responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+      }
+    },     
+     {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: false,
+      }
+    },        
+    
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+    ]
+    });
+    
+    //feedback slider;
+    $('.feedback-slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+          responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+      }
+    }
+    ]
+    });
+    
+    //funfact counter;
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+    
+    
+    //brand-slider;
+    $('.brand-slider').slick({
+        slidesToShow : 5,
+        centerMode : true,
+        centerPadding : '0px',
+        prevArrow : '<i class="fas fa-chevron-left prevBtn"></i>',
+        nextArrow : '<i class="fas fa-chevron-right nextBtn"></i>',
+         responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: false,
+      }
+    },         
+             
+    ]
     });
 });
